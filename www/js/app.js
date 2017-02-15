48a32658ea85862753f6ad95b93f6335c20c4dec
pizzaernesto <Napoli26>
2017-02-15 14:52:26 +0100

    aggiunta del metodo hide() dello splashscreen

--- www/js/app.js
+++ www/js/app.js
@@ -25,6 +25,7 @@ angular.module('app', ['ionic', 'app.controllers','camera', 'app.routes', 'app.d
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
+    navigator.splashscreen.hide();
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
